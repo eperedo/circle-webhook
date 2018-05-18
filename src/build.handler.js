5a5e1dc8dfeeb15d0096dfc02416d274ce2a8727
Eduardo Peredo <jrperedo@gmail.com>
2018-05-18 12:58:59 -0500

    feat(build): Emit deployment event only on success status

--- src/build.handler.js
+++ src/build.handler.js
@@ -5,10 +5,10 @@ const deploymentBranches = ['dev', 'production'];
 async function handler(request, h) {
 	const { branch, status, reponame } = request.payload.payload;
 	// console.log('PAYLOAD', JSON.stringify(request.payload));
-	console.log('Deployment to ', branch, status, reponame);
+	console.log('Build Info', branch, status, reponame);
 	if (status === 'success') {
 		if (deploymentBranches.includes(branch)) {
-			// console.log('Deployment to ', branch, status, reponame);
+			console.log('Deployment to ', branch, status, reponame);
 			return h.response();
 		}
 	}
