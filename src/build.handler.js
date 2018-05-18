cf8a3bd8584aa502af652f3dd0daf5f2abc197f8
Eduardo Peredo <jrperedo@gmail.com>
2018-05-18 13:40:34 -0500

    feat(build): run pm2 deployment

--- src/build.handler.js
+++ src/build.handler.js
@@ -1,14 +1,25 @@
 'use strict';
 
+const { spawn } = require('child_process');
+// const program = require('commander');
 const deploymentBranches = ['dev', 'production'];
+const pathProjects = '/Users/fullfactory/workspace';
 
 async function handler(request, h) {
 	const { branch, status, reponame } = request.payload.payload;
-	// console.log('PAYLOAD', JSON.stringify(request.payload));
-	console.log('Build Info', branch, status, reponame);
 	if (status === 'success') {
 		if (deploymentBranches.includes(branch)) {
-			console.log('Deployment to ', branch, status, reponame);
+			const deploymentFile = `${pathProjects}/${reponame}/deployment.json`;
+			const deploySh = spawn('sh', ['deployment.sh', deploymentFile, branch], {
+				cwd: __dirname,
+				env: Object.assign({}, process.env, { PATH: process.env.PATH + ':/usr/local/bin' }),
+			});
+			deploySh.stdout.on('data', (w) => {
+				console.log('D', w.toString());
+			});
+			deploySh.on('close', (code) => {
+				console.log('code', code);
+			});
 			return h.response();
 		}
 	}
