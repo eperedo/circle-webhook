a12e2033fa45070005bbd1cb0e0810d509ea81c9
Eduardo Peredo <jrperedo@gmail.com>
2018-05-18 12:21:34 -0500

    fix(build): Added bash script

--- src/build.handler.js
+++ src/build.handler.js
@@ -1,8 +1,8 @@
 'use strict';
 
 async function handler(request, h) {
-	console.log('PAYLOAD', request.payload);
-	return h.response(request.payload);
+	console.log('PAYLOAD', JSON.stringify(request.payload));
+	return h.response(request.payload.payload);
 }
 
 module.exports = handler;
