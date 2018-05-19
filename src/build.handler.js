3f254e89abc88ca87128a4d9640ddf48d24e006a
Eduardo Peredo <jrperedo@gmail.com>
2018-05-19 16:06:17 -0500

    feat(build): adding deployment after successful build

--- src/build.handler.js
+++ src/build.handler.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('dotenv').config();
 const axios = require('axios');
 const { spawn } = require('child_process');
 const deploymentBranches = ['dev', 'production'];
@@ -8,44 +9,60 @@ const homePath = process.env.HOME;
 const projectsPath = `${homePath}/node`;
 const githubAppRunn = 'https://github.com/apprunn';
 
+const deploySh = spawn('sh', ['deployment.sh'], {
+	cwd: __dirname,
+	env: Object.assign({}, process.env, {
+		PATH: process.env.PATH + ':/usr/local/bin',
+	}),
+});
+
+deploySh.stdout.on('data', w => console.log(w));
+
+deploySh.stderr.on('data', w => console.log(w.toString()));
+
+deploySh.on('close', code => {
+	console.log('Exit code', code);
+});
+
 async function handler(request, h) {
 	const { branch, status, reponame } = request.payload.payload;
-	console.log(branch, status, reponame);
-	// if (status === 'success' && allowedRepos.includes(reponame)) {
-	// 	if (deploymentBranches.includes(branch)) {
-	// 		const deploymentFile = `${projectsPath}/${reponame}/source/deployment.json`;
-	// 		const deploySh = spawn('sh', ['deployment.sh', deploymentFile, branch], {
-	// 			cwd: __dirname,
-	// 			env: Object.assign({}, process.env, { PATH: process.env.PATH + ':/usr/local/bin' }),
-	// 		});
-	// 		deploySh.on('close', async (code) => {
-	// 			const slackMessage = {
-	// 				fallback: `New Deployment on ${reponame}`,
-	// 				color: 'good',
-	// 				fields: [
-	// 					{
-	// 						title: 'Repository',
-	// 						value: `<${githubAppRunn}/${reponame}|${reponame}>`,
-	// 						short: false,
-	// 					},
-	// 					{
-	// 						title: 'Branch',
-	// 						value: `<${githubAppRunn}/${reponame}/tree/${branch}|${branch}>`,
-	// 						short: false,
-	// 					},
-	// 				],
-	// 			};
-	// 			if (code === 0) {
-	// 				slackMessage.text = `Deployment in ${reponame} to branch: ${branch} on ${new Date()}`;
-	// 			} else {
-	// 				slackMessage.color = 'danger';
-	// 				slackMessage.text = `Deployment failed: ${reponame} to branch: ${branch} on ${new Date()}`;
-	// 			}
-	// 			await axios.post(process.env.SLACK_WEBHOOK_URL, slackMessage);
-	// 		});
-	// 		return h.response();
-	// 	}
-	// }
+	if (status === 'success' && allowedRepos.includes(reponame)) {
+		if (deploymentBranches.includes(branch)) {
+			const deploymentFile = `${projectsPath}/${reponame}/source/deployment.json`;
+			const deploySh = spawn('sh', ['deployment.sh'], {
+				cwd: __dirname,
+				env: Object.assign({}, process.env, {
+					PATH: process.env.PATH + ':/usr/local/bin',
+				}),
+			});
+			deploySh.on('close', async code => {
+				const slackMessage = {
+					fallback: `New Deployment on ${reponame}`,
+					color: 'good',
+					fields: [
+						{
+							title: 'Repository',
+							value: `<${githubAppRunn}/${reponame}|${reponame}>`,
+							short: false,
+						},
+						{
+							title: 'Branch',
+							value: `<${githubAppRunn}/${reponame}/tree/${branch}|${branch}>`,
+							short: false,
+						},
+					],
+				};
+				if (code === 0) {
+					slackMessage.text = `Deployment in ${reponame} to branch: ${branch} on ${new Date()}`;
+				} else {
+					slackMessage.color = 'danger';
+					slackMessage.text = `Deployment failed: ${reponame} to branch: ${branch} on ${new Date()}`;
+				}
+				await axios.post(process.env.SLACK_WEBHOOK_URL, slackMessage);
+			});
+			return h.response();
+		}
+	}
 	return h.response();
 }
 
