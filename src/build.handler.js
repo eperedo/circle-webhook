6748e391e77b1cb7a1ca2951141386bc608c2c1c
Eduardo Peredo <jrperedo@gmail.com>
2018-05-19 16:24:09 -0500

    feat(build): agregado config de dotenv

--- src/build.handler.js
+++ src/build.handler.js
@@ -1,6 +1,5 @@
 'use strict';
 
-require('dotenv').config();
 const axios = require('axios');
 const { spawn } = require('child_process');
 const deploymentBranches = ['dev', 'production'];
@@ -9,32 +8,21 @@ const homePath = process.env.HOME;
 const projectsPath = `${homePath}/node`;
 const githubAppRunn = 'https://github.com/apprunn';
 
-const deploySh = spawn('sh', ['deployment.sh'], {
-	cwd: __dirname,
-	env: Object.assign({}, process.env, {
-		PATH: process.env.PATH + ':/usr/local/bin',
-	}),
-});
-
-deploySh.stdout.on('data', w => console.log('PM2', w.toString()));
-
-deploySh.stderr.on('data', w => console.log(w.toString()));
-
-deploySh.on('close', code => {
-	console.log('Exit code', code);
-});
-
 async function handler(request, h) {
 	const { branch, status, reponame } = request.payload.payload;
 	if (status === 'success' && allowedRepos.includes(reponame)) {
 		if (deploymentBranches.includes(branch)) {
 			const deploymentFile = `${projectsPath}/${reponame}/source/deployment.json`;
-			const deploySh = spawn('sh', ['deployment.sh'], {
+			const deploySh = spawn('sh', ['deployment.sh', deploymentFile, branch], {
 				cwd: __dirname,
 				env: Object.assign({}, process.env, {
 					PATH: process.env.PATH + ':/usr/local/bin',
 				}),
 			});
+
+			deploySh.stdout.on('data', w => console.log('PM2', w.toString()));
+
+			deploySh.stderr.on('data', w => console.log('PM2-ERROR', w.toString()));
 			deploySh.on('close', async code => {
 				const slackMessage = {
 					fallback: `New Deployment on ${reponame}`,
